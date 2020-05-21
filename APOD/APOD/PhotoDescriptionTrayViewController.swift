d1480bf04ede249348eb1b77a769c956d6e4e6ab
Shawn Gee <shawngee@gmail.com>
2020-05-21 18:38:38 -0400

    Implemented pan gesture and animation to allow tray to open and close nicely

---
+++ APOD/APOD/PhotoDescriptionTrayViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoDescriptionTrayViewController.swift
+//  APOD
+//
+//  Created by Shawn Gee on 5/21/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDescriptionTrayViewController: UIViewController {
+
+}
