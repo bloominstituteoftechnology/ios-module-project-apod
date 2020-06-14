26e1bba8401139f36287b4c41d4b400763aede88
David Wright <davidtwright89@gmail.com>
2020-06-14 16:44:26 -0400

    Added an APODCollectionViewCell class

---
+++ Astronomy POD/Astronomy POD/Views/APODCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  APODCollectionViewCell.swift
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class APODCollectionViewCell: UICollectionViewCell {
+    
+}
