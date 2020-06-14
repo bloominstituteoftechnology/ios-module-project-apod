0ca50cb01cc99c23efe8fe0823f9334739c534c7
David Wright <davidtwright89@gmail.com>
2020-06-14 16:32:23 -0400

    Added a category to NSURL called Secure

---
+++ Astronomy POD/Astronomy POD/Utililties/NSURL+Secure.m
@@ -0,0 +1,13 @@
+//
+//  NSURL+Secure.m
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "NSURL+Secure.h"
+
+@implementation NSURL (Secure)
+
+@end
