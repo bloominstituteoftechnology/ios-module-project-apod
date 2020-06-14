f4b56027c57a93c650566a56234845844b37abfd
David Wright <davidtwright89@gmail.com>
2020-06-14 16:41:45 -0400

    Setup the public interface for APODCache

--- Astronomy POD/Astronomy POD/Model/APODCache.h
+++ Astronomy POD/Astronomy POD/Model/APODCache.h
@@ -8,10 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
+@interface APODCache<Value> : NSObject
 
-@interface APODCache : NSObject
+- (void)cacheValue:(nonnull Value)value ForKey:(nonnull NSString *)key;
+- (nullable Value)valueForKey:(nonnull NSString *)key;
+- (void)clear;
 
 @end
-
-NS_ASSUME_NONNULL_END
