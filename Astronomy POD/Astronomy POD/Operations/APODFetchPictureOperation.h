84960bb88c8d71c4d81702a863bbc3eace3ab105
David Wright <davidtwright89@gmail.com>
2020-06-14 16:11:39 -0400

    Added an APODFetchPictureOperation class

---
+++ Astronomy POD/Astronomy POD/Operations/APODFetchPictureOperation.h
@@ -0,0 +1,17 @@
+//
+//  APODFetchPictureOperation.h
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APODFetchPictureOperation : NSOperation
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Astronomy POD/Astronomy POD/Operations/APODFetchPictureOperation.m
@@ -0,0 +1,13 @@
+//
+//  APODFetchPictureOperation.m
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "APODFetchPictureOperation.h"
+
+@implementation APODFetchPictureOperation
+
+@end
