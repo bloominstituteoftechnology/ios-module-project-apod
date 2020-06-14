5ce77d3b05c3b46c8f2767dc1df01d4dbaca91e1
David Wright <davidtwright89@gmail.com>
2020-06-14 16:42:01 -0400

    Implemented APODCache

--- Astronomy POD/Astronomy POD/Model/APODCache.m
+++ Astronomy POD/Astronomy POD/Model/APODCache.m
@@ -8,6 +8,45 @@
 
 #import "APODCache.h"
 
+@interface APODCache ()
+
+@property (copy, readwrite) NSMutableDictionary<NSString *, id> *cache;
+@property (copy, readonly) dispatch_queue_t queue;
+
+@end
+
+
 @implementation APODCache
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _queue = dispatch_queue_create([@"AstronomyPODCacheQueue" UTF8String], nil);
+    }
+    return self;
+}
+
+- (void)cacheValue:(id)value ForKey:(nonnull NSString *)key
+{
+    dispatch_async(self.queue, ^{
+        [self.cache setObject:value forKey:key];
+    });
+}
+
+- (id)valueForKey:(nonnull NSString *)key
+{
+    __block id value;
+    dispatch_sync(self.queue, ^{
+        value = [self.cache objectForKey:key];
+    });
+    return value;
+}
+
+- (void)clear
+{
+    dispatch_async(self.queue, ^{
+        [self.cache removeAllObjects];
+    });
+}
 
 @end
