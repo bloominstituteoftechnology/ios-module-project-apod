38b3f42f63566a7b4995f430eeed91a89d542b1d
David Wright <davidtwright89@gmail.com>
2020-06-14 19:41:42 -0400

    Implemented APODCollectionViewController

--- Astronomy POD/Astronomy POD/View Controllers/APODCollectionViewController.swift
+++ Astronomy POD/Astronomy POD/View Controllers/APODCollectionViewController.swift
@@ -8,23 +8,265 @@
 
 import UIKit
 
+private let reuseIdentifier = "APODCell"
+
 class APODCollectionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    private let client = APODClient()
+    private let fetchPictureQueue = OperationQueue()
+    private var operations = [String: Operation]()
+    private let cache = APODCache<UIImage>()
+    private let minimumCellSpacing: CGFloat = 0.0
+    
+    private var currentDate: Date {
+        Date()
     }
     
+    private var displayDate = Date() {
+        didSet {
+            if currentDate != oldValue {
+                cache.clear()
+                
+                var date = displayDate.startOfMonth
+                
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+                
+                for _ in 1...displayDate.numberOfDaysInMonth {
+                    
+                    let dateString = dateFormatter.string(from: date)
+                    
+                    client.fetchPOD(withDateString: dateString) { (pictureReference, error) in
+                        if let error = error { NSLog("Error fetching picture for date: \(dateString): \(error)"); return }
+                        
+                        guard let pictureReference = pictureReference else { return }
+                        
+                        self.pictureReferences.append(pictureReference)
+                    }
+                    
+                    guard let nextDate = Calendar.current.date(byAdding: .day, value: 1, to: date) else { continue }
+                    date = nextDate
+                }
+                
+                updateTitleView()
+                collectionView?.reloadData()
+            }
+        }
+    }
+    
+    private var pictureReferences = [APODPicture]() {
+        didSet {
+            collectionView?.reloadData()
+        }
+    }
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var monthLabel: UILabel!
+    
+    // MARK: - View Controller Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateTitleView()
 
-    /*
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action:  #selector(refresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
+    }
+    
+    @objc func refresh() {
+        collectionView.reloadData()
+        collectionView.refreshControl?.endRefreshing()
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetail" {
+            guard let indexPath = collectionView.indexPathsForSelectedItems?.first,
+                let detailVC = segue.destination as? APODDetailViewController else { return }
+            
+            detailVC.pictureReference = pictureReferences[indexPath.item]
+            
+            guard let cell = collectionView.cellForItem(at: indexPath) as? APODCollectionViewCell,
+                let image = cell.imageView.image else { return }
+            detailVC.image = image
+        }
     }
-    */
+    
+    // MARK: - IBActions
+    
+    @IBAction func nextMonthButtonTapped(_ sender: UIButton) {
+        guard let nextDisplayDate = Calendar.current.date(byAdding: .month, value: 1, to: displayDate) else { return }
+        displayDate = nextDisplayDate
+    }
+    
+    @IBAction func previousMonthButtonTapped(_ sender: UIButton) {
+        guard let nextDisplayDate = Calendar.current.date(byAdding: .month, value: -1, to: displayDate) else { return }
+        displayDate = nextDisplayDate
+    }
+    
+    // MARK: - Private Methods
+    
+    private func updateTitleView() {
+        guard isViewLoaded else { return }
+        monthLabel.text = currentDate.monthName
+    }
+    
+    private func loadImage(forCell cell: APODCollectionViewCell, forItemAt indexPath: IndexPath) {
+        let pictureReference = pictureReferences[indexPath.item]
+        
+        if let image = cache.value(forKey: pictureReference.dateString) {
+            cell.imageView.image = image;
+            return
+        }
+        
+        guard let url = URL(string: pictureReference.urlString) else { return }
+        
+        let fetchOp = APODFetchPictureOperation(pictureURL: url)
+        
+        let cacheOp = BlockOperation {
+            if let image = fetchOp.image {
+                self.cache.cacheValue(image, forKey: pictureReference.dateString)
+            }
+        }
+        
+        let completionOp = BlockOperation {
+            defer { self.operations.removeValue(forKey: pictureReference.dateString) }
+            
+            if let currentIndexPath = self.collectionView.indexPath(for: cell),
+                currentIndexPath != indexPath {
+                return // Cell has been reused
+            }
+            
+            if let image = fetchOp.image {
+                cell.imageView.image = image
+            }
+        }
+        
+        cacheOp.addDependency(fetchOp)
+        completionOp.addDependency(fetchOp)
+        
+        fetchPictureQueue.addOperation(fetchOp)
+        fetchPictureQueue.addOperation(cacheOp)
+        OperationQueue.main.addOperation(completionOp)
+        
+        operations[pictureReference.dateString] = fetchOp
+    }
+    
+    private func date(from dateString: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let date = dateFormatter.date(from: dateString)
+        
+        return date
+    }
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension APODCollectionViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictureReferences.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! APODCollectionViewCell
+        
+        let dateString = pictureReferences[indexPath.item].dateString
+        let pictureDate = date(from: dateString)!
+        let calendar = Calendar.current
+        let dayComponent = calendar.dateComponents([.day], from: pictureDate)
+        let day = dayComponent.day!
+        let dayString = String(format: "%02d", day)
+        
+        cell.dateLabel.text = dayString
+        
+        loadImage(forCell: cell, forItemAt: indexPath)
+    
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected item #: \(indexPath.item)")
+        self.performSegue(withIdentifier: "ShowDetail", sender: self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if pictureReferences.count > 0 {
+            let pictureRef = pictureReferences[indexPath.item]
+            operations[pictureRef.dateString]?.cancel()
+        } else {
+            for (_, operation) in operations {
+                operation.cancel()
+            }
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension APODCollectionViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var totalUsableWidth = collectionView.frame.width
+        let inset = self.collectionView(collectionView, layout: collectionViewLayout, insetForSectionAt: indexPath.section)
+        totalUsableWidth -= inset.left + inset.right
+        
+        let minWidth: CGFloat = 150.0
+        let numberOfItemsInOneRow = Int(totalUsableWidth / minWidth)
+        totalUsableWidth -= CGFloat(numberOfItemsInOneRow - 1) * minimumCellSpacing
+        let width = totalUsableWidth / CGFloat(numberOfItemsInOneRow)
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: minimumCellSpacing, bottom: 0, right: minimumCellSpacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        minimumCellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        minimumCellSpacing
+    }
+}
+
+
+extension Date {
+    var startOfMonth: Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: self)
+
+        return  calendar.date(from: components)!
+    }
+    
+    var dayOfMonth: Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: self)
+
+        return  components.day!
+    }
+    
+    var numberOfDaysInMonth: Int {
+        let calendar = Calendar.current
+        let interval = calendar.dateInterval(of: .month, for: self)!
+        
+        return calendar.dateComponents([.day], from: interval.start, to: interval.end).day!
+    }
+    
+    var monthName: String {
+        let monthFormatter = DateFormatter()
+        monthFormatter.dateFormat = "LLLL"
+        let monthName = monthFormatter.string(from: self)
+        
+        return monthName
+    }
 }
