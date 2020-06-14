af0211fd0ab72bc3176e7a5b94400d5f5535934d
David Wright <davidtwright89@gmail.com>
2020-06-14 16:36:21 -0400

    Added an APODCache class

---
+++ Astronomy POD/Astronomy POD/Model/APODCache.m
@@ -0,0 +1,13 @@
+//
+//  APODCache.m
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "APODCache.h"
+
+@implementation APODCache
+
+@end
