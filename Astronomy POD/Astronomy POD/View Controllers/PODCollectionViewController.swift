73a80afe474ad3fd3c8f1891c581b373bd0417f7
David Wright <davidtwright89@gmail.com>
2020-06-14 15:03:20 -0400

    Added a PODCollectionViewController class and created a bridging header

---
+++ Astronomy POD/Astronomy POD/View Controllers/PODCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PODCollectionViewController.swift
+//  Astronomy POD
+//
+//  Created by David Wright on 6/14/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class PODCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
