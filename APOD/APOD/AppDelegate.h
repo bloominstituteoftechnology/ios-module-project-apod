2d08a9742c4442bd0cf5070f1c5356672bbcea44
Chris Dobek <dobek.christopher@gmail.com>
2020-06-18 15:12:28 -0400

    APOD project created

---
+++ APOD/APOD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APOD
+//
+//  Created by Chris Dobek on 6/18/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ APOD/APOD/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  APOD
+//
+//  Created by Chris Dobek on 6/18/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
