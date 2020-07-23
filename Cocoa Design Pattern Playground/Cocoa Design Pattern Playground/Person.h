dcaf987fd91c7c5252d7abfafa51a70db7cf4f1e
nonye <nnezekwo@gmail.com>
2020-07-23 10:21:22 -0700

    Added a Person model class

---
+++ Cocoa Design Pattern Playground/Cocoa Design Pattern Playground/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  Cocoa Design Pattern Playground
+//
+//  Created by Nonye on 7/23/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Cocoa Design Pattern Playground/Cocoa Design Pattern Playground/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Cocoa Design Pattern Playground
+//
+//  Created by Nonye on 7/23/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
