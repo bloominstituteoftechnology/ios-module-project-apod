e9ce39073ad8122c11fcaeed47fe73111059f7f8
Chad Parker <chad@chadparker.com>
2020-08-13 19:06:26 -0700

    Create Xcode project

---
+++ AstronomyPicOfTheDay/AstronomyPicOfTheDay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AstronomyPicOfTheDay
+//
+//  Created by Chad Parker on 8/13/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AstronomyPicOfTheDay/AstronomyPicOfTheDay/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AstronomyPicOfTheDay
+//
+//  Created by Chad Parker on 8/13/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
