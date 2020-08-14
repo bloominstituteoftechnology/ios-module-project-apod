57798c4dd6ecc357ad94ae7b01d36b6ba33c3fdb
Claudia Maciel <clc_80@yahoo.com>
2020-08-13 20:26:51 -0700

    Added Picture Model Class

---
+++ Daily_Astronomy_Picture/Daily_Astronomy_Picture/Picture.h
@@ -0,0 +1,17 @@
+//
+//  Picture.h
+//  Daily_Astronomy_Picture
+//
+//  Created by Claudia Maciel on 8/13/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Picture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
