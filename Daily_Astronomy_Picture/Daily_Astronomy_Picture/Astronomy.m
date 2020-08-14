072826f5e4f8bea895db71c3f22f4934bae3bb06
Claudia Maciel <clc_80@yahoo.com>
2020-08-13 20:22:40 -0700

    Added Astronomy Model Class

---
+++ Daily_Astronomy_Picture/Daily_Astronomy_Picture/Astronomy.m
@@ -0,0 +1,13 @@
+//
+//  Astronomy.m
+//  Daily_Astronomy_Picture
+//
+//  Created by Claudia Maciel on 8/13/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "Astronomy.h"
+
+@implementation Astronomy
+
+@end
