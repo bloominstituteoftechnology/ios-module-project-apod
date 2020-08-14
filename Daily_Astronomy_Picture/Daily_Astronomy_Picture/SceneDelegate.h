bbe018e725ff6b69424a6d797b9b4dc65bf0275d
Claudia Maciel <clc_80@yahoo.com>
2020-08-13 18:50:31 -0700

    Added Xcode project

---
+++ Daily_Astronomy_Picture/Daily_Astronomy_Picture/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Daily_Astronomy_Picture
+//
+//  Created by Claudia Maciel on 8/13/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Daily_Astronomy_Picture/Daily_Astronomy_Picture/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Daily_Astronomy_Picture
+//
+//  Created by Claudia Maciel on 8/13/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
