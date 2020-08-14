ba09631fe0d38eb4f403349954c1b0bcc902e7fe
Chad Parker <chad@chadparker.com>
2020-08-13 21:11:32 -0700

    Update PicOfTheDay with Inits

--- AstronomyPicOfTheDay/AstronomyPicOfTheDay/Model/PicOfTheDay.h
+++ AstronomyPicOfTheDay/AstronomyPicOfTheDay/Model/PicOfTheDay.h
@@ -12,12 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PicOfTheDay : NSObject
 
-@property (nonatomic, copy) NSString *copyright;
-@property (nonatomic, copy) NSString *dateString;
-@property (nonatomic, copy) NSString *explanation;
-@property (nonatomic, copy) NSString *mediaType;
-@property (nonatomic, copy) NSString *title;
-@property (nonatomic, copy) NSString *urlString;
+@property (nonatomic, readonly, copy) NSString *copyright;
+@property (nonatomic, readonly, copy) NSString *dateString;
+@property (nonatomic, readonly, copy) NSString *explanation;
+@property (nonatomic, readonly, copy) NSString *mediaType;
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy) NSString *urlString;
+
+- (instancetype)initWithCopyright:(NSString *)copyright
+                       dateString:(NSString *)dateString
+                      explanation:(NSString *)explanation
+                        mediaType:(NSString *)mediaType
+                            title:(NSString *)title
+                       urlString:(NSString *)urlString;
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
 
--- AstronomyPicOfTheDay/AstronomyPicOfTheDay/Model/PicOfTheDay.m
+++ AstronomyPicOfTheDay/AstronomyPicOfTheDay/Model/PicOfTheDay.m
@@ -10,4 +10,50 @@
 
 @implementation PicOfTheDay
 
+- (instancetype)initWithCopyright:(NSString *)copyright
+                       dateString:(NSString *)dateString
+                      explanation:(NSString *)explanation
+                        mediaType:(NSString *)mediaType
+                            title:(NSString *)title
+                       urlString:(NSString *)urlString
+{
+    if (self = [super init]) {
+        _copyright = copyright;
+        _dateString = dateString;
+        _explanation = explanation;
+        _mediaType = mediaType;
+        _title = title;
+        _urlString = urlString;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSString *copyright = dictionary[@"copyright"];
+    if (![copyright isKindOfClass:NSString.class]) return nil;
+
+    NSString *dateString = dictionary[@"date"];
+    if (![dateString isKindOfClass:NSString.class]) return nil;
+
+    NSString *explanation = dictionary[@"explanation"];
+    if (![explanation isKindOfClass:NSString.class]) return nil;
+
+    NSString *mediaType = dictionary[@"media_type"];
+    if (![mediaType isKindOfClass:NSString.class]) return nil;
+
+    NSString *title = dictionary[@"title"];
+    if (![title isKindOfClass:NSString.class]) return nil;
+
+    NSString *urlString = dictionary[@"url"];
+    if (![urlString isKindOfClass:NSString.class]) return nil;
+
+    return [self initWithCopyright:copyright
+                        dateString:dateString
+                       explanation:explanation
+                         mediaType:mediaType
+                             title:title
+                         urlString:urlString];
+}
+
 @end
