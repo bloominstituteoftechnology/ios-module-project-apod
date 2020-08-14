105e0adba6f2b4d4f4d838d83d278607ba96810c
Claudia Maciel <clc_80@yahoo.com>
2020-08-13 20:30:34 -0700

    Declared and implemented the Picture Model

--- Daily_Astronomy_Picture/Daily_Astronomy_Picture/Picture.h
+++ Daily_Astronomy_Picture/Daily_Astronomy_Picture/Picture.h
@@ -12,6 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Picture : NSObject
 
+@property (nonatomic, copy, readonly) NSString *dateString;
+@property (nonatomic, copy, readonly) NSString *urlString;
+@property (nonatomic, copy, readonly) NSString *title;
+@property (nonatomic, copy, readonly) NSString *explanation;
+@property (nonatomic, copy, readonly) NSString *mediaType;
+
+- (nonnull instancetype)initWithDateString:(NSString *)dateString
+                                 urlString:(NSString *)urlString
+                                     title:(NSString *)title
+                               explanation:(NSString *)explanation
+                                 mediaType:(NSString *)mediaType;
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
