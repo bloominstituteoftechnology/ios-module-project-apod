d899149c7b6c5ab359f226e8c4d3203982e26d61
kensam1 <samarakodykennedy@gmail.com>
2020-10-08 23:26:48 -0400

    Created the project

---
+++ Astronomy/Astronomy/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Astronomy
+//
+//  Created by Lambda_School_Loaner_241 on 10/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Astronomy/Astronomy/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Astronomy
+//
+//  Created by Lambda_School_Loaner_241 on 10/8/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
